f3214a12b09f709d98d6288a6c590390fe65f3e6
FatihTheDev <130474375+FatihTheDev@users.noreply.github.com>
2026-01-28 15:01:19 +0000

    Update kali-postinstall.sh

--- kali-postinstall.sh
+++ kali-postinstall.sh
@@ -1,9 +1,9 @@
-sudo apt-get purge kali-desktop-core kali-desktop-xfce --allow-remove-essential
+sudo apt-get purge -y kali-desktop-core kali-desktop-xfce --allow-remove-essential
 
-sudo apt purge firefox-esr
+sudo apt purge -y firefox-esr
 
 sudo apt-mark manual xfce4 xfce4-goodies lightdm network-manager network-manager-gnome
 
-sudo apt purge firefox-esr
+sudo apt purge -y firefox-esr
 
-sudo apt install --reinstall xfce4 xfce4-goodies lightdm network-manager
+sudo apt install --reinstall -y xfce4 xfce4-goodies lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings network-manager
