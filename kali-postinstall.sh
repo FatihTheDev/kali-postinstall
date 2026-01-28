7f38c5f920f3269904c7f4e2bf8bc99dc00d0865
FatihTheDev <130474375+FatihTheDev@users.noreply.github.com>
2026-01-28 12:44:15 +0000

    Add files via upload

---
+++ kali-postinstall.sh
@@ -0,0 +1,9 @@
+sudo apt-get purge kali-desktop-core kali-desktop-xfce --allow-remove-essential
+
+sudo apt purge firefox-esr
+
+sudo apt-mark manual xfce4 xfce4-goodies lightdm network-manager network-manager-gnome
+
+sudo apt purge firefox-esr
+
+sudo apt install --reinstall xfce4 xfce4-goodies lightdm network-manager
